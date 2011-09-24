326ad24f5bfc0d72c2eef356774333705485e077
Chris Barts <chbarts@gmail.com>
2011-09-24 00:08:05 -0600

    Added utf8encode-test.c, which generates all non-astral-plane characters.

---
+++ utf8encode-test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "utf8encode.h"
+
+int main(void)
+{
+    uint32_t cdpt;
+    uint8_t buf[6];
+    int n, i;
+
+    for (cdpt = 0; cdpt <= 0xffff; cdpt++) {
+        n = utf8encode(cdpt, buf);
+        printf("%04x: %d: ", cdpt, n);
+        for (i = 0; i < n; i++)
+            printf("%02x ", buf[i]);
+        fwrite(buf, 1, n, stdout);
+        puts("");
+    }
+
+    return 0;
+}
