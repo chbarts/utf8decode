91aa6e0daf1bab88c6aa8904bdc3d0a40336c23c
Chris Barts <chbarts@gmail.com>
2017-03-14 11:39:19 -0600

    Add rot32768.c

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC=gcc
 CFLAGS=-O3 -march=native -flto
 DEPS=utf8encode.h utf8decode.h parse_codepoint.h handle_ferr.h 
 
-all: codepoint2utf8 codepoint-list codepoint-range utf8encode-test utf8file utf8parse isutf8
+all: codepoint2utf8 codepoint-list codepoint-range utf8encode-test utf8file utf8parse isutf8 rot32768
 
 codepoint2utf8: codepoint2utf8.c parse_codepoint.c utf8encode.c
 	$(CC) -o codepoint2utf8 codepoint2utf8.c parse_codepoint.c utf8encode.c $(CFLAGS)
@@ -25,5 +25,8 @@ utf8parse: utf8parse.c utf8decode.c
 isutf8: isutf8.c handle_ferr.c utf8decode.c
 	$(CC) -o isutf8 isutf8.c handle_ferr.c utf8decode.c $(CFLAGS)
 
+rot32768: rot32768.c handle_ferr.c utf8decode.c utf8encode.c
+	$(CC) -o rot32768 rot32768.c handle_ferr.c utf8decode.c utf8encode.c $(CFLAGS)
+
 clean:
-	rm codepoint2utf8 codepoint-list codepoint-range utf8encode-test utf8file utf8parse isutf8
+	rm codepoint2utf8 codepoint-list codepoint-range utf8encode-test utf8file utf8parse isutf8 rot32768
---
+++ rot32768.c
@@ -0,0 +1,108 @@
+/*
+
+Copyright © 2017, Chris Barts.
+
+This file is part of utf8decode.
+
+utf8decode is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+utf8decode is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with utf8decode.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "handle_ferr.h"
+#include "utf8decode.h"
+#include "utf8encode.h"
+
+static uint32_t rot(uint32_t in)
+{
+   uint32_t res;
+   int plane;
+
+   plane = in/65536;
+   res = (in + 32768) % 65536;
+   return (res + plane*65536);
+}
+
+static void dofile(FILE *inf, char name[])
+{
+   unsigned char seq[7];
+   uint32_t cdpt;
+   size_t n = 0;
+   int len;
+
+   memset(seq, '\0', sizeof(seq) * sizeof(seq[0]));
+
+   while (next_sequence(inf, seq) != -1) {
+      n += seqlen(seq[0]);
+      sequence_to_ucs4(seq, &cdpt);
+      cdpt = rot(cdpt);
+      len = utf8encode(cdpt, (uint8_t *) seq);
+      fwrite(seq, 1, len, stdout);
+   }
+
+    if (!valid_sequence(seq) && !feof(fin))
+        fprintf(stderr, "rot32768: invalid UTF-8 sequence at %llu in %s\n",
+                (unsigned long long int) n, fname);
+}
+
+int main(int argc, char *argv[])
+{
+   FILE *inf;
+   int i = 1;
+
+   if (1 == argc) {
+      dofile(stdin, "stdin");
+      return 0;
+   }
+
+   if (!strcmp(argv[1], "--")) {
+      i = 2;
+      goto loop;
+   }
+
+   if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h")) {
+      printf("usage: %s [files...]\n", argv[0]);
+      puts("Rotates Unicode codepoints (UTF-8 encoded only) such that they stay within their plane.");
+      puts("Self-reversible, the same way rot13 is");
+      return 0;
+   }
+
+   if (!strcmp(argv[1], "--version") || !strcmp(argv[1], "-v")) {
+      puts("rot32768 version 1.0");
+      puts("Chris Barts, 2017. Released under the GPL Version 3.0 or later");
+      return 0;
+   }
+
+loop:
+   for (; i < argc; i++) {
+      if ((inf = fopen(argv[i], "rb")) == NULL) {
+         handle_ferr(argv[i], "rot32768");
+         continue;
+      }
+
+      dofile(inf, argv[i]);
+
+      if (ferror(inf)) {
+         handle_ferr(argv[i], "rot32768");
+         fclose(inf);
+         continue;
+      }
+
+      if (fclose(inf) == EOF)
+         handle_ferr(argv[i], "rot32768");
+   }
+
+   return 0;
+}
